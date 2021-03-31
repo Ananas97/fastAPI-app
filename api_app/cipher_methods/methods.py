220691df7313679d4089d422439f9cbf071e4ce7
Ananas97 <ana.rei.014@gmail.com>
2021-03-31 23:38:44 +0200

    add encode + decode methods to api and code ducumented

--- api_app/cipher_methods/methods.py
+++ api_app/cipher_methods/methods.py
@@ -1,43 +1,108 @@
 from typing import Optional
 import string
-from .data import data, helper
+
+''' Implemented Caesar Cipher'''
+
 
 class CaesarCipher:
     def __init__(self, shift: Optional[int] = 3, message: string = ''):
         self.shift = shift
         self.message = message
-        self.unranks = helper.unrank_symbols()
+        self.unranks = unrank_symbols()
+
+    '''Check whether message public field is within range of accepted input'''
 
     def check_input_message(self):
         try:
             for char in self.message:
-                if char not in data.ALLOWED_SYMBOLS:
+                if char not in ALLOWED_SYMBOLS:
                     raise Exception('InputOutOfRange')
                 else:
                     pass
         except Exception as e:
             print(type(e), e.args)
+        finally:
+            return self
+
+    '''Check whether shift public field is within range of accepted input'''
+
     def check_input_shift(self):
         try:
-                if self.shift < 1:
-                    raise Exception('InputOutOfRange')
-                else:
-                    pass
+            if self.shift < 1:
+                raise Exception('InputOutOfRange')
+            else:
+                pass
         except Exception as e:
             print(type(e), e.args)
+        finally:
+            return self
+
+    '''Encrypt method for Caesar cipher'''
 
     def encrypt(self):
         encrypted = ''
         self.check_input_message().check_input_shift()
-        for char in range(len(self.message)):
-            index = helper.find_rank(char, self.unranks) + self.shift
-            encrypted += self.unranks[index]
+        for char in self.message:
+            index = self.unranks.index(char)
+            index = index + self.shift
+            encrypted = encrypted + self.unranks[index]
         return encrypted
+
+    '''Decrypt method for Caesar cipher'''
 
     def decrypt(self):
         decrypted = ''
-        self.check_input_message().check_input_shift()
-        for char in range(len(self.message)):
-            index = helper.find_rank(char, self.unranks) - self.shift
-            decrypted += self.unranks[index]
+        self.check_input_message() \
+            .check_input_shift()
+        for char in self.message:
+            index = self.unranks.index(char) - self.shift
+            decrypted = decrypted + self.unranks[index]
         return decrypted
+
+
+'''below constants fro allowed symbols and temp lists for decoded ASCII chars'''
+ALLOWED_PUNCTUATION = string.punctuation
+ALLOWED_WHITESPACE = string.whitespace
+ALLOWED_LATIN_UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ALLOWED_LATIN_LOWER = ALLOWED_LATIN_UPPER.lower()
+
+ALLOWED_SYMBOLS = ALLOWED_PUNCTUATION + ALLOWED_WHITESPACE + ALLOWED_LATIN_UPPER + ALLOWED_LATIN_LOWER
+
+ranks = []
+unranks = []
+
+'''Helper Function to have ranked(with ASCII codes) list of expected symbols'''
+
+
+def rank_symbols():
+    for symbol in ALLOWED_SYMBOLS:
+        ranks.append(ord(symbol))
+    ranks.sort()
+    return ranks
+
+
+'''Function to have unranked(without ASCII codes) list of expected symbols'''
+
+
+def unrank_symbols():
+    ranks = rank_symbols()
+    for rank in ranks:
+        unranks.append(chr(rank))
+    return unranks
+
+
+'''Function to find rank of char in a list'''
+
+
+def find_rank(char, list):
+    rank = 0
+    for l in list:
+        if char == l:
+            rank = list.index(l)
+    return rank
+
+
+'''Uncomment a below code block yo test how encrypt method works'''
+# if __name__ == "__main__":
+#     cipher = CaesarCipher(3, 'akaLJHGHmar')
+#     print(cipher.encrypt())
--- api_app/fastapi_engine.py
+++ api_app/fastapi_engine.py
@@ -3,9 +3,12 @@ import secrets
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi.responses import HTMLResponse
+from cipher_methods.methods import CaesarCipher as Cipher
 
 app = FastAPI()
 security = HTTPBasic()
+
+'''Method with BasicAuth for user authorization'''
 
 
 def valid(credentials: HTTPBasicCredentials = Depends(security)):
@@ -20,21 +23,42 @@ def valid(credentials: HTTPBasicCredentials = Depends(security)):
     return True
 
 
+'''Root routing  with basic info on methods'''
+
+
 @app.get("/", dependencies=[Depends(valid)])
 async def get_root():
     html_content = "<html>" \
-           "<body>" \
-           "<h1>FASTAPI APP</h1>" \
-           "<div>" \
-           "Route for decode: <a href='/decode'>/decode/</a>" \
-           "</div>" \
-           "<div>" \
-           "Route for encode: <a href='/encode'>/encode/</a>" \
-           "</div>" \
-           "</body>" \
-           "</html>"
+                   "<body>" \
+                   "<h1>FASTAPI APP</h1>" \
+                   "<div>" \
+                   "Route for decode: <a href='/decode'>/decode/</a>" \
+                   "</div>" \
+                   "<div>" \
+                   "Route for encode: <a href='/encode'>/encode/</a>" \
+                   "</div>" \
+                   "</body>" \
+                   "</html>"
 
     return HTMLResponse(content=html_content, status_code=200)
+
+
+'''Root routing for decode method'''
+
+
+@app.get("/decode/{shift}/{message}", dependencies=[Depends(valid)])
+async def decode(message: str, shift: int):
+    cipher = Cipher(shift, message)
+    return {"decoded": cipher.decrypt()}
+
+
+'''Root routing for encode method'''
+
+
+@app.get("/encode/{shift}/{message}", dependencies=[Depends(valid)])
+async def encode(message: str, shift: int):
+    cipher = Cipher(shift, message)
+    return {"encrypted": cipher.encrypt()}
 
 
 uvicorn.run(app, host="127.0.0.1", port=8000)
